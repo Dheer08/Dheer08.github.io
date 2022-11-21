66998af1bb5d2e693c7dfe8d3940e696c2ab71ff
RakshithDheer08 <rakshithdheer.kunduru@psiog.com>
2022-11-21 16:56:28 +0530

    rec

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,11 +1,5 @@
-
-function ClickSubmit(e)
-{
-    e.preventDefault();
-    grecaptcha.ready(function() {
-      grecaptcha.execute('6Ld7VyIjAAAAACZ86buFRtsfol62JVqAsUbWZmtY', {action: 'submit'}).then(function(token) {
-          // Add your logic to submit to your backend server here.
-      });
-    });
+function ClickSubmit()
+{  
     console.log("demo Recapche");
+    window.location.href("thankyou.html")
 }
