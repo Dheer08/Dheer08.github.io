f19b27a3203b9c69fc3e59c7d9c6e0703a2a40dc
RakshithDheer08 <rakshithdheer.kunduru@psiog.com>
2023-01-01 23:14:04 +0530

    Service

---
+++ action.php
@@ -0,0 +1,42 @@
+<?php
+
+if(isset($_POST['submit']))
+{
+
+function CheckCaptcha($userResponse) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => 6LcEbSIjAAAAALGBIpcfp0iaZvMQwKswbu1efDvE
+            'response' => $userResponse
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($res, true);
+    }
+
+
+    // Call the function CheckCaptcha
+    $result = CheckCaptcha($_POST['g-recaptcha-response']);
+
+    if ($result['success']) {
+        //If the user has checked the Captcha box
+        echo "Captcha verified Successfully";
+	
+    } else {
+        // If the CAPTCHA box wasn't checked
+       echo '<script>alert("Error Message");</script>';
+    }
+}
+    ?>
+    
